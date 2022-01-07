3be002094152afc723cd5fd0b40c7658d7996275
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2022-01-07 22:05:48 +0100

    Setup environmental variables

--- Social_Email_Login_NodeJS/models/User.js
+++ Social_Email_Login_NodeJS/models/User.js
@@ -1,6 +1,15 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
+    social_id: {
+        type: String,
+      },
+    social_provider: {
+        type: String
+      },
+    social_photo_url: {
+        type: String
+      },
     name: {
         type : String,
         required : true
